be12aa238adbd7166abfe652a4d3db74c8f5e4ce
Doosies <song961003@gmail.com>
2023-05-10 21:47:09 +0900

    first

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { act } from 'react-dom/test-utils';
 
-test('renders learn react link', () => {
+test('ui 최초 렌더링 확인', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const inputButton = screen.getByText(/입력 해주세요/i);
+
+  // expect(linkElement).toBeInTheDocument();
 });
+
+it('sfs', () => {
+  
+})
