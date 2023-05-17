8032030c5e608c6175a454971645b0b8a9734daa
Doosies <song961003@gmail.com>
2023-05-17 18:59:12 +0900

    m: todoapp에서는 그려지는걸 확인

--- src/TodoApp.test.tsx
+++ src/TodoApp.test.tsx
@@ -1,21 +1,23 @@
-import { screen } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import userEvent from "@testing-library/user-event";
 import TodoApp from "./TodoApp"
-import useMockTodoContext from "./hooks/useMockTodoContext";
+import { TodoProvider } from "./TodoContext";
+import { mockTodos } from "./hooks/useMockTodoContext";
 
 describe('<TodoApp/>', () => {
 
   const useSetup = () => {
-    const {
-      mockTodos, mockAddTodo, mockDeleteTodo
-    } = useMockTodoContext(<TodoApp/>);
+    render(
+      <TodoProvider init={mockTodos}>
+        <TodoApp/>
+      </TodoProvider>
+    );
 
     const input = screen.getByTestId("TodoFormInput");
     const submitButton = screen.getByTestId('TodoFormButton')
     const todoList = screen.getByTestId('TodoList');
 
     return {
-      mockTodos, mockAddTodo, mockDeleteTodo, 
       input, submitButton, todoList
     };
   }
@@ -25,12 +27,25 @@ describe('<TodoApp/>', () => {
     expect(todoList).toBeInTheDocument();
   });
 
-  it('input 입력 후 todo 추가버튼 눌렀을 때 추가 되는지 테스트', async() => {
-    const {input, submitButton, mockAddTodo} = useSetup();
-
-    await userEvent.type(input, "new todo");
+  it('input 입력 후 todo 추가버튼 눌렀을 때 추가 되는지 테스트', async () => {
+    const { input, submitButton } = useSetup();
+    const newTodo = "new todo";
+  
+    await userEvent.type(input, newTodo);
     await userEvent.click(submitButton);
 
-    expect(mockAddTodo).toBeCalledWith("new todo");
+    const newTodoComponent = screen.getByText("new todo")
+    expect(newTodoComponent).toBeInTheDocument();
+  
+  });
+
+  it('삭제버튼 눌렀을때 해당 todo가 screen에서 지워지는지', async () => {
+    useSetup();
+    const firstDeleteButton = screen.getAllByTestId("DeleteButton")[0];
+
+    await userEvent.click(firstDeleteButton);
+
+    const beforeItem = screen.queryByText(mockTodos[0].value);
+    expect(beforeItem).not.toBeInTheDocument();
   });
 });
